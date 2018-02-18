e7ec8b4f5b8552fce08486564f4d62cbd1028398
Gonzodamus <Dick@TheDickWard.com>
2018-02-18 07:56:47 -0500

    images are loading! Just need to make sure they're in batches and implement the stop. Then styling

---
+++ async-fiesta.js
@@ -0,0 +1,41 @@
+$(() => {
+  const $bucket = $(".photo-bucket");
+  const draw = img => $bucket.append(img);
+
+  // NOTE: The height and width variables can be changed to fetch different sized images.
+  const getImageUrl = id =>
+    `https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/rotate=deg:0/resize=width:100,height:100,fit:crop/output=format:jpg,quality:95/compress/${id}`;
+
+  const startLoading = () => {
+    let batchStart = 0;
+    while (batchStart + 5 < IMAGE_IDS.length) {
+      load(batchStart);
+
+      batchStart += 5;
+    }
+
+    // get first five in images.js array
+    //  iterate through and create those image objects
+    // then draw them
+    // TODO: Implement me.
+    console.log("Start!");
+  };
+
+  const load = batchStart => {
+    let imgNum = batchStart;
+    while (imgNum < batchStart + 5) {
+      const img = new Image();
+      img.src = getImageUrl(IMAGE_IDS[imgNum]);
+      draw(img);
+      imgNum++;
+    }
+  };
+
+  const stopLoading = () => {
+    // TODO: Implement me.
+    console.log("Stop!");
+  };
+
+  $("button.start").on("click", startLoading);
+  $("button.stop").on("click", stopLoading);
+});
